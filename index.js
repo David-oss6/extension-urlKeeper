95929300d5a782974e1613c863575cacdf490803
David Iglesias <im.david85@gmail.com>
2025-04-14 23:26:05 +0200

    ok

--- index.js
+++ index.js
@@ -6,22 +6,58 @@ function empty() {
 
 function searchLocal() {
     let list = localStorage.getItem('list')
+    list ? list = JSON.parse(list) : []
     return list
 }
 
+function printList(list) {
+    ulList.innerHTML = '';
+    list.forEach(element => {
+        let li = document.createElement('li');
+        let ref = document.createElement('h2')
+        let url = document.createElement('p')
+        let txt = document.createElement('p')
+        let delBtn = document.createElement('button')
+        ref.textContent(element.ref)
+        url.textContent(element.url)
+        txt.textContent(element.text)
+        li.appendChild(ref, url, p2, delBtn)
+        ulList.appendChild(li);
+    });
+    list.forEach(element => {
+        let li = document.createElement('li');
+        li.textContent = url;
+        ulList.appendChild(li);
+    });
+}
+
 function initialSearch() {
-    console.log('initial')
-    let list = localStorage.getItem('list')
+    let list = searchLocal()
+    list ? printList(list) : ""
 }
 
 function create() {
-    let myUrl = document.URL
-    //  let ref = myUrl.slice(7)
-    //   ref = ref.split('/')[0];
-    //   let txt = document.querySelector('#input-txt').value
-
     let list = searchLocal()
-    list ? localStorage.setItem('list', myUrl) : localStorage.setItem('list', `${list}, ${myUrl}`)
+
+    let myUrl = document.URL
+    let ref = myUrl.slice(7)
+    ref = ref.split('.')[0];
+    let txt = document.querySelector('#input-txt').value
+    if (!txt) { txt = "" }
+
+    let merch = {
+        url: myUrl,
+        ref: ref,
+        text: txt,
+    }
+    console.log(merch)
+    merch = JSON.stringify(merch)
+
+    console.log(merch)
+    console.log(JSON.parse(merch))
+
+    printList(list)
+
 
 }
 initialSearch()
